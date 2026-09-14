a49994b89e43a583410028221fc3dcbecd42499a
Alejandro Hernández Cordero <ahcorde@gmail.com>
2026-09-14 14:50:40 +0200

    Fix warning (#335)
    * Fix warning
    
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    
    * Removed warnigs
    
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    
    ---------
    
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- src/message_filters/__init__.py
+++ src/message_filters/__init__.py
@@ -481,25 +481,16 @@ class TimeSequencer(SimpleFilter):
         """
         Construct a TimeSequencer filter for a subscriber.
 
-        Args:
-        -----
-        input_filter (SimpleFilter)
-            The input filter to connect to. Typically a Subscriber.
-        delay (Duration | float)
-            The delay (in seconds) to wait for messages to arrive before
+        :param input_filter: The input filter to connect to. Typically a Subscriber.
+        :param delay: The delay (in seconds) to wait for messages to arrive before
             dispatching them.
-        update_rate (Duration | float)
-            The rate at which to check for messages that are ready to be
+        :param update_rate: The rate at which to check for messages that are ready to be
             dispatched.
-        queue_size (int)
-            The maximum number of messages to store. Set 0 for no limit.
-        node (Node)
-            The node to create the timer on.
-        msg_stamp_attr (str, optional)
-            The attribute to use for retrieving the timestamp from the message.
-            Should point to a builtin_interfaces.msg.Time field.
-            Defaults to "header.stamp".
-
+        :param queue_size: The maximum number of messages to store. Set 0 for no limit.
+        :param node: The node to create the timer on.
+        :param msg_stamp_attr: The attribute to use for retrieving the timestamp from the
+            message. Should point to a builtin_interfaces.msg.Time field.
+            Defaults to ``'header.stamp'``.
         """
         super().__init__()
         if not isinstance(delay, Duration):
